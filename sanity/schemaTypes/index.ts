cbe06e1ecf9362671b032fa639bb290be1bf6130
BenDev <armandbenjamin30@gmail.com>
2025-04-13 15:09:59 +0200

    Add Sanity

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,22 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import { studentType } from "./studentType"
+import { moduleType } from './moduleType'
+import { instructorType } from './instructorType'
+import { lessonCompletionType } from './lessonCompletionType'
+import { lessonType } from './lessonType'
+import { enrollmentType } from './enrollmentType'
+import { blockContent } from './blockContent'
+import { courseType } from './courseType'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    studentType,
+    moduleType,
+    instructorType,
+    lessonCompletionType,
+    lessonType,
+    enrollmentType,
+    blockContent,
+    courseType,
+  ],
+};
